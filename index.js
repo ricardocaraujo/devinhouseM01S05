c014cc251cf3afac4b76a346b979294519575a4a
ricardo <rcavaraujo@gmail.com>
2022-07-30 13:53:15 -0300

    [M1S05] - Ex 2 - Criando módulos

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-import { saudacao } from "./saudacao.js";
+//import { saudacao } from "./saudacao.js";
+import saudacaoEspecial from "./saudacao-especial.js";
 
 //const saudacao = require('./saudacao.js')
 
-const mensagem = saudacao();
+//const mensagem = saudacao();
+
+const mensagem = saudacaoEspecial("Ricardo")
 
 console.log(mensagem)
